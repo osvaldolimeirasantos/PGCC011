66f309deade1b031539de9a3638b36fa35634017
osvaldolimeirasantos <91644823+osvaldolimeirasantos@users.noreply.github.com>
2021-10-25 21:33:01 -0300

    Update triplas_pitagoricas_c_serial.cpp

--- triplas_pitagoricas_c_serial.cpp
+++ triplas_pitagoricas_c_serial.cpp
@@ -1,43 +1,29 @@
-#include <omp.h>
 #include <stdio.h>
-
 
 void tripla_pitagorica_c(int quantidade)
 {
-    // inicializando as variáveis que vão compor o terno pitagórico
-    int a, b, c = 0;
-    // inicializando m e n que vão ser utilizadas para realizar a interação dos valores.
-    int m = 2;
-    int n;
-    int contador = 0; // a variável contador, será utilizada para trazer o total de triplas pitagóricas existentes.
-    int parada = 0;
-   
-   //enquanto o valor de parada for 0 a função dentro do while vai continuar rodando.
-    while (parada == 0) {
-        // nesse loop, verificamos se n é menor que m, caso a condição seja verdadeira, o cálculo será executado.
-        for (n=1; n < m; ++n) {
-            //cálculo para obter os valores de a,b e c
-            a = (m * m) - (n * n);
-            b = 2 * (m * n);
-            c = (m * m) + (n * n);
-        
-             //a tripla pitagórica compreende que: a<b<c , desta forma como c é o maior valor entre as outras 2 partes,
-            // verificamos se c é maior que o valor da quantidade inserida pelo usuário.
-            if (c > quantidade)
-                parada = 1; //caso a condição seja verdadeira, a execução é encerrada e o valor do contador é apresentado ao usuário.
-            
-            ++contador; // a cada tripla encontrada o contador é incrementado +1.
+    int a, b, c, i, j, k;
+    int contador = 0;
+       
+    for (i = 1; i < quantidade - 1; i++){
+        a = i * i;
+        for (j = i + 1; j < quantidade; j++){
+            b = j * j;
+            for (k = j + 1; k <= quantidade; k++){
+                c = k * k;
+                if (a + b == c){
+                    contador++;
+                }
+            }
         }
-        m++;
     }
-    printf("%d", contador);
+
+printf("%d", contador);
 }
  
 // Driver Code
 int main()
 {
-    int quantidade = 400000000;
+    int quantidade = 50;
     tripla_pitagorica_c(quantidade);
 }
-
-
